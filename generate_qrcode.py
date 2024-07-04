6e7aa52cf42d9feacec9b02e9f307b59e093a646
Vinicius Soares <vinisoarescastro@gmail.com>
2024-07-04 20:12:25 -0300

    refresh

--- generate_qrcode.py
+++ generate_qrcode.py
@@ -2,8 +2,8 @@ import qrcode
 import os
 
 # Dados do aluno
-nome = "Marcos Araujo"
-matricula = "12346789"
+nome = "Aluno Teste"
+matricula = "1234678"
 
 # Cria o texto que será armazenado no QR Code
 texto = f"Nome: {nome}, Matrícula: {matricula}"
