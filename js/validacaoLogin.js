19fb44f4b77dbdad24207bcb72251be1f6fb5d6d
Vinicius Soares <vinisoarescastro@gmail.com>
2024-10-09 12:26:36 -0300

    login page inicial

---
+++ js/validacaoLogin.js
@@ -0,0 +1,17 @@
+window.document.getElementById('loginForm').addEventListener('submit', function(login) {
+    login.preventDefault();
+
+    const emailCpf = document.querySelector('#emailCpf').value;
+    const password = document.querySelector('#password').value;
+    const msgErro = document.querySelector('#msg-erro');
+
+    const validEmailCpf = '12345678900';
+    const validPassword = '12345678';
+
+    if (emailCpf === validEmailCpf && password === validPassword) {
+        window.location.href = 'index-page-principal.html';
+    } else {
+        msgErro.innerHTML = 'Email/CPF ou senha inválidos. Tente novamente!';
+    }
+
+})
